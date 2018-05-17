e4ab693a106b32766768029eae30b57b429beb6f
Skaronea <skaroneae@bodhiudevl01.udev.six3>
2018-05-17 12:53:46 -0600

    Redid the entire excercise - corrected packages on Java files

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -1,7 +1,7 @@
-package main.java.hello;
+package hello;
 
 public class Greeter{
     public String sayHello(){
-        return "Hello World!";
+        return "Hello there!";
     }
 }
--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -1,14 +1,10 @@
-package main.java.hello;
+package hello;
 
-import org.joda.time.LocalTime;
 
 public class HelloWorld{
     public static void main(String[] args) {
-        LocalTime currentTime = new LocalTime();
-        System.out.println("The current local time is: " + currentTime);
+        Greeter greet = new Greeter();
 
-        
-        Greeter greeter = new Greeter();
-        System.out.println(greeter.sayHello());
+        System.out.println(greet.sayHello());
     }
 }
